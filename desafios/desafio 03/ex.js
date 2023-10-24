5799bc74894439aa6899a97e12ae858b54be6dbf
Matheus Henrique <131389471+CloneTz@users.noreply.github.com>
2023-10-24 15:50:05 -0300

    Update ex.js

---
+++ desafios/desafio 03/ex.js
@@ -0,0 +1,7 @@
+let num = prompt("Digite um número para verificar se é PAR ou ÍMPAR!");
+
+if (Number(num % 2 == 0)) {
+  alert(`O número ${num} é PAR`);
+} else {
+  alert(`O número ${num} é ÍMPAR`);
+}
