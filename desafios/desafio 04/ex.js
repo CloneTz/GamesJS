3a4c22ddaef81d2a352be6a1d33ccc7670d8a7db
Matheus Henrique <131389471+CloneTz@users.noreply.github.com>
2023-10-26 11:44:14 -0300

    exercicios

---
+++ desafios/desafio 04/ex.js
@@ -0,0 +1,51 @@
+/*
+CONTADOR DE 1 A 10
+
+let contador = 0;
+
+while (contador < 10) {
+  contador++;
+  console.log(contador);
+}
+
+*/
+
+/*
+CONTADOR DE 10 A 0
+
+let contador = 11;
+
+while (contador > 0) {
+  contador--;
+  console.log(contador);
+}
+
+*/
+
+/*
+CONTAGEM REGRESSIVA
+
+let pergunta = prompt(
+  "A partir de que número você quer começar a contagem regressiva?"
+);
+let num;
+num = pergunta;
+
+while (num >= 0) {
+  console.log(`${num}`);
+  num--;
+}
+
+*/
+
+let pergunta = prompt(
+  "A partir de que número você quer começar a contagem regressiva?"
+);
+let num;
+num = pergunta;
+contador = 0;
+
+while (contador <= num) {
+  console.log(`${num}`);
+  contador++;
+}
